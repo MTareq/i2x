0baad1e6a267f75962165c86fc323701f4ffda8a
mtareq <m.tareq89@gmail.com>
2017-02-22 14:43:40 +0200

    Removed sqlite from prod settings for heruko

--- i2x_challenge/settings.py
+++ i2x_challenge/settings.py
@@ -95,12 +95,12 @@ WSGI_APPLICATION = 'i2x_challenge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Internationalization

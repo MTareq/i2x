c8be0a078dba4c69f01f04e44b7c28cd20da3dbc
Mtareq <m.tareq89@gmail.com>
2017-02-22 13:25:16 +0200

    Some components and helper methods for auth

--- frontend/js/index.jsx
+++ frontend/js/index.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {} from './auth.jsx';
 import { Router, Route, hashHistory, Link, IndexRoute} from 'react-router';
 
 
+function requireAuth(nextState, replace) {
+    if (!auth.loggedIn()) {
+        replace({
+            pathname:'/app/login/',
+            state: {nextPathname: '/app/'}
+        })
+    }
+}
+
+
 class App extends React.Component{
+    
+    constructor(props) {
+    super(props);
+    this.state = { 'user':[] };
+    }
+
+    componentDidMount() {
+        this.loadUserData()
+    }
+
+    contextTypes= {router: React.PropTypes.object.isRequired}
+
+    logoutHandler(){
+        auth.logout()
+        this.context.router.replace('/')
+    }
+
+    loadUserData(){
+        $.ajax({
+            method: 'GET',
+            url: '/api/users/i/',
+            datatype: 'json',
+            headers: {
+                'Authorization': 'Token ' + localStorage.token
+            },
+            success: function(res) {
+                this.setState(user: res)
+            }.bind(this)
+        })
+    }
+
     render(){
         return(
             <div class="container-fluid">
@@ -15,6 +57,10 @@ class App extends React.Component{
                 <div class="row">
                     <div class="col-lg-12">
                         {this.props.children}
+                        <div>
+                            <h1>You are now logged in, {this.state.user.username}</h1>
+                            <button onClick={this.logoutHandler}>Log out</button>
+                        </div>
                     </div>
                 </div>
                 <div></div>
@@ -23,13 +69,34 @@ class App extends React.Component{
                 <div></div>
             </div>
         )
-    }
+    };
 }
 
-class Landing extends React.Component{
+class Login extends React.Component{
+    constructor(props) {
+    super(props);
+        this.refs = props;
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+
+        var username = this.refs.username.value
+        var pass = this.refs.pass.value
+
+        auth.login(username, pass, (loggedIn) => {
+            if (loggedIn) {
+                this.context.router.replace('/')
+            }
+        })
+    }
+
     render(){
-        return(
-            <div>Hello Landing</div>
+        return ( <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="username" ref="username" />
+                <input type="password" placeholder="password" ref="pass" />
+                <input type="submit" />
+            </form>
         )
     }
 }
@@ -61,7 +128,7 @@ const root = document.getElementById('root');
 
 ReactDOM.render(<Router history={hashHistory}>
                     <Route path="/" component={App}>
-                        <IndexRoute component={Landing}></IndexRoute>
+                        <IndexRoute component={Login}></IndexRoute>
                         <Route path="user" component={User}></Route>
                         <Route path="team" component={Team}></Route>
                         <Route path="new" component={NewUser}></Route>
